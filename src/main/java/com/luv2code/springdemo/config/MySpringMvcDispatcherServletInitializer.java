aa9ba927b91a7857fcc130109357c94933966f2c
Manohar Bhat <overtomanu@outlook.com>
2021-06-11 17:04:17 +0530

    removed auto generated comment

--- src/main/java/com/luv2code/springdemo/config/MySpringMvcDispatcherServletInitializer.java
+++ src/main/java/com/luv2code/springdemo/config/MySpringMvcDispatcherServletInitializer.java
@@ -13,7 +13,6 @@ public class MySpringMvcDispatcherServletInitializer extends AbstractAnnotationC
 
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
